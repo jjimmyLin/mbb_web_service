fe6cfcacfae75d855e4fb85fc50ca0bdb663c578
jjimmyLin <iaminhi3124@gmail.com>
2025-06-03 21:27:04 +1000

    debug

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -1,8 +1,8 @@
 import { UserManager } from "oidc-client-ts";
 
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_cP8fqjk9I",
-    client_id: "5nco85v2rs1m8p377ged607bjh",
+    authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_04DE9zl9z",
+    client_id: "lvki4cf3c5cjsrif7jtduimrm",
     redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
     response_type: "code",
     scope: "phone openid email"
@@ -14,8 +14,8 @@ export const userManager = new UserManager({
 });
 
 export async function signOutRedirect () {
-    const clientId = "5nco85v2rs1m8p377ged607bjh";
+    const clientId = "lvki4cf3c5cjsrif7jtduimrm";
     const logoutUri = "https://d84l1y8p4kdic.cloudfront.net";
-    const cognitoDomain = "https://mbb-login.auth.ap-southeast-2.amazoncognito.com";
+    const cognitoDomain = "https://ap-southeast-204de9zl9z.auth.ap-southeast-2.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
