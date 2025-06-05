e82442dd4d558cadc4daf1e908bc0469ebf2aa13
jjimmyLin <iaminhi3124@gmail.com>
2025-06-05 17:19:29 +1000

    solved localhost token receive problem enable local functioning and coding

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -1,9 +1,12 @@
 import { UserManager } from "oidc-client-ts";
 
+const isLocalhost = window.location.hostname === "localhost";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_04DE9zl9z",
   client_id: "lvki4cf3c5cjsrif7jtduimrm",
-  redirect_uri: "https://d3ssa7u9tniekd.cloudfront.net/callback",
+  redirect_uri: isLocalhost
+    ? "http://localhost:5173/callback"
+    : "https://d3ssa7u9tniekd.cloudfront.net/callback",
   response_type: "code",
   scope: "email openid phone",
 };
@@ -14,7 +17,9 @@ export const userManager = new UserManager({
 
 export async function signOutRedirect() {
   const clientId = "lvki4cf3c5cjsrif7jtduimrm";
-  const logoutUri = "https://d3ssa7u9tniekd.cloudfront.net";
+  const logoutUri = isLocalhost
+    ? "http://localhost:5173"
+    : "https://d3ssa7u9tniekd.cloudfront.net";
   const cognitoDomain = "https://ap-southeast-204de9zl9z.auth.ap-southeast-2.amazoncognito.com";
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
